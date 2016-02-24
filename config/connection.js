975dad04f448324103edfa80a40d80487568dce3
Ivan <ivan.baena04@gmail.com>
2016-02-23 20:36:04 -0500

    add

--- config/connection.js
+++ config/connection.js
@@ -1,4 +1,10 @@
 var mysql = require('mysql');
 var connection = mysql.createConnection(process.env.JAWSDB_URL);
 
-module.exports = connection.connect();
+connection.query('SELECT 1 + 1 AS solution', function(err, rows, fields) {
+ if (err) throw err;
+
+ console.log('The solution is: ', rows[0].solution);
+});
+
+module.exports = connection;
