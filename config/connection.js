05128f8c3154bb53390fca197f384e04453c2f53
Ivan <ivan.baena04@gmail.com>
2016-02-23 20:17:28 -0500

    add

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,7 @@
 var mysql = require('mysql');
 var connection = mysql.createConnection({
     port: 3306,
-    host: 'mysql://b2454f095d0fac:3ef1d3d1@us-cdbr-iron-east-03.cleardb.net/heroku_10c4b9077b310a3',
+    host: 'mysql://b2454f095d0fac:3ef1d3d1@us-cdbr-iron-east-03.cleardb.net',
     database: 'heroku_10c4b9077b310a3'
 });
 
