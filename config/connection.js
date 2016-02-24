71d9b57f14c3798c334166da3faf219becb8b8af
Ivan <ivan.baena04@gmail.com>
2016-02-23 20:51:57 -0500

    test

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,6 @@
 
 var mysql = require('mysql');
-var connection = mysql.createConnection({
-    port: 3306,
-    host: 'us-cdbr-iron-east-03.cleardb.net/heroku_10c4b9077b310a3',
-    user:'b2454f095d0fac',
-    password: '3ef1d3d1',
-    database: 'heroku_10c4b9077b310a3'
-});
+var connection = mysql.createConnection(process.env.JAWSDB_URL);
 
 connection.connect(function(err) {
     if (err) {
