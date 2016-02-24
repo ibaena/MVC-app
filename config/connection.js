2af54e46baa3bb805d02b948f86c86f9f2ed76b4
Ivan <ivan.baena04@gmail.com>
2016-02-23 19:48:09 -0500

    add

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,7 @@
 var mysql = require('mysql');
 var connection = mysql.createConnection({
     port: 3306,
-    host: 'localhost',
+    host: 'b2454f095d0fac:3ef1d3d1@us-cdbr-iron-east-03.cleardb.net/heroku_10c4b9077b310a3',
     user: 'root',
     password: '@pril2488',
     database: 'burger_list'
