b8609b224a851910946b223ad3dc5b3ca49f8c98
Ivan <ivan.baena04@gmail.com>
2016-02-23 20:47:21 -0500

    add

--- config/connection.js
+++ config/connection.js
@@ -1,10 +1,19 @@
+
 var mysql = require('mysql');
-var connection = mysql.createConnection(process.env.JAWSDB_URL);
+var connection = mysql.createConnection({
+    port: 3306,
+    host: 'us-cdbr-iron-east-03.cleardb.net/heroku_10c4b9077b310a3',
+    user:'b2454f095d0fac',
+    password: '3ef1d3d1',
+    database: 'heroku_10c4b9077b310a3'
+});
 
-connection.query('SELECT 1 + 1 AS solution', function(err, rows, fields) {
- if (err) throw err;
-
- console.log('The solution is: ', rows[0].solution);
+connection.connect(function(err) {
+    if (err) {
+        console.error('error connecting: ' + err.stack);
+        return;
+    }
+    console.log('connected as id ' + connection.threadId);
 });
 
 module.exports = connection;
